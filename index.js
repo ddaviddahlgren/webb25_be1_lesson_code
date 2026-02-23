5673bfa11e047d9d3bfa9291fbf23975873cf186
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-02-23 11:54:04 +0100

    add put request

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 let artists = [
   { id: 1, name: 'Bad Bunny' },
@@ -64,6 +64,29 @@ app.post("/api/artists", (req, res) => {
 
     artists.push(artist)
     return res.status(201).json(artist)
+})
+
+app.put("api/artists/:id", (req, res) => {
+  const id = Number(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: "ID must be a number."
+    })
+  }
+  const artist = artists.find((a) => a.id === id)
+  if (!artist){
+    return res.status(404).json({
+      message: "Artist does not exist."
+    })
+  }
+  const { name } = req.body
+  if (!name || typeof name !== "string"){
+    return res.status(400).json({
+      message: "A name is required."
+    })
+  }
+  artist.name = name
+  return res.json(artist  )
 })
 
 app.listen(PORT,(error) => {
