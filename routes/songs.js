7009ba8fcd848e34e39cf85f7e12e44869874b9b
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-02-26 13:06:30 +0100

    add basic router file

---
+++ routes/songs.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router()
+
+let songs = [
+  { id: 1, title: 'Espresso', artist: 'Sabrina Carpenter' },
+  { id: 2, title: 'Creep', artist: 'Radiohead' },
+  { id: 3, title: 'Tití Me Preguntó', artist: 'Bad Bunny' },
+];
